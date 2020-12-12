88358f5eb719a9000e87cbe96e08881e11bbd5e8
umar-ism <umarismaildjanoff@gmail.com>
2020-12-12 19:26:37 +0500

    create music player

---
+++ app.js
@@ -0,0 +1,9 @@
+function change() {
+    document.getElementById("pause").src = "https://icons-for-free.com/iconfiles/png/512/media+music+pause+player+sound+video+icon-1320085967989908413.png"
+    document.getElementById("myAudio").onclick.play()
+}
+
+
+
+
+    
